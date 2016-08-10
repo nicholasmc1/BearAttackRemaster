980f510c9a76fe47663e1d5ec51bae5338e961f1
Nicholas McDonnell <nicholasmcdonnell1@gmail.com>
2016-08-10 17:52:37 +1000

    Project cleanup, forest generator works now. Simplified forest system for easier rebuilding

--- BearAttack_Unity/Assets/Scripts/makeLevel.cs
+++ BearAttack_Unity/Assets/Scripts/makeLevel.cs
@@ -6,14 +6,9 @@ public class makeLevel : MonoBehaviour {
 	public GameObject deforestPrefab;
 	
 	public GameObject forestPrefab;
-	public GameObject flowerPrefab;
-	public GameObject mushroomPrefab;
-	public GameObject redBushPrefab;
-	public GameObject blueBushPrefab;
-	
+
 	public GameObject groundPrefab;
 	public GameObject deforestGroundPrefab;
-	public GameObject grassPrefab;
 	public GameObject forestEdgePrefab;
 	
 	// Use this for initialization
@@ -59,30 +54,7 @@ public class makeLevel : MonoBehaviour {
 	private GameObject randomForest()
 	{
 		int choice = Random.Range(0,101);
-		
-		if(choice >= 90)
-		{
-			return mushroomPrefab;
-		}
-		
-		if(choice < 90 && choice >= 70)
-		{
-			return flowerPrefab;
-		}
-		
-		if(choice < 70 && choice >= 67)
-		{
-			return redBushPrefab;
-		}
-		
-		if(choice < 67 && choice >= 65)
-		{
-			return blueBushPrefab;
-		}
-		
-		else
-		{
-			return forestPrefab;
-		}
+
+		return forestPrefab;
 	}
 }
